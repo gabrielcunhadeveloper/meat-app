0e3dae33e829d072ca9ff2abefc20ea3c2150928
gabriel <gabrielcunhadev@gmail.com>
2020-01-08 11:29:09 -0300

    Commit initial.

---
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import{ ActivatedRoute } from '@angular/router'
+import { RestaurantsService } from '../restaurants/restaurants.service'
+import { Restaurant } from 'app/restaurants/restaurant/restaurant.model';
+
+@Component({
+  selector: 'mt-restaurant-detail',
+  templateUrl: './restaurant-detail.component.html',
+  styleUrls: ['./restaurant-detail.component.css']
+})
+export class RestaurantDetailComponent implements OnInit {
+
+   restaurant: Restaurant
+
+  constructor(private restaurantService: RestaurantsService,
+              private route: ActivatedRoute) {
+
+  }
+
+  ngOnInit() {   
+    this.restaurantService.restaurantById(this.route.snapshot.params['id'])
+      .subscribe(restaurantPorId => this.restaurant = restaurantPorId) 
+  }
+
+}
